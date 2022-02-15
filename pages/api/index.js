4f5bfca0e36527ebcd8d7123e4e90498264ec088
raficraft <raficraft@gmail.com>
2022-02-15 20:59:47 +0100

    push for vercel hosting step9

--- pages/api/index.js
+++ pages/api/index.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
   const request = req.body;
 
   //process.cwd  has Current Write directory
-  const src = path.join(__dirname, request.dir);
+  const src = path.join(serverRuntimeConfig.PROJECT_ROOT, request.dir);
 
   try {
     const files = await promises.readdir(src);
