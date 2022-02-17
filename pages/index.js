fa382de595800b8f44b3bc99a216bc66257918fb
raficraft <raficraft@gmail.com>
2022-02-17 14:05:43 +0100

    responsve home page is done

--- pages/index.js
+++ pages/index.js
@@ -7,9 +7,11 @@ import Image from "next/image";
 
 import logo from "./../public/assets/svg/1.5x/logo.png";
 import { ModalContext } from "../engine/context/modal/ModalProvider";
+import Index_content from "./Index_content";
 
 export default function Home() {
   const { openModal } = useContext(ModalContext);
+
   /*** */
 
   function handleClick(e) {
@@ -55,47 +57,7 @@ export default function Home() {
           </div>
         </Heroes>
         <section className="content">
-          <article className="article">
-            <h1>A quoi sert la Géobiologie ?</h1>
-            <p>
-              La géobiologie étudie les impacts de l’environnement sur la santé
-              des vivants, c’est-à-dire non seulement des humains, mais aussi
-              des animaux et des végétaux. Le but de cette pratique est de
-              mettre en évidence les perturbations que les cours d’eau, les
-              cavités vides, les zones de failles, les cheminées
-              cosmo-telluriques, les réseaux Hartmann et Curry et autres
-              phénomènes géologiques présentent à proximité de l’habitation
-              peuvent provoquer.
-            </p>
-            <hr></hr>
-            <h2>Pourquoi une expertise Géobiologique ?</h2>
-            <p>
-              L’exposition à long terme aux diverses nuisances présentes dans
-              l’environnement peut entraîner des maladies chroniques, des
-              allergies, des troubles du sommeil ou même une dépression. Une
-              expertise géobiologique permet de déterminer quels sont réellement
-              les phénomènes qui nuisent au bien-être d’une habitation pour
-              ensuite trouver des corrections adéquates. Grâce à ses facultés
-              techniques et sensorielles, le spécialiste en géobiologie est en
-              mesure de neutraliser les effets des phénomènes et ondes nocives.
-              Une expertise géobiologique contribue ainsi à la résolution des
-              problèmes physiologiques que les occupants d’une maison peuvent
-              avoir. Avant d’entamer des travaux de construction et de
-              rénovation, demander une expertise en géobiologie permet de
-              connaitre l’emplacement idéal pour les pièces et les modifications
-              à apporter à l’habitation pour que la qualité de vie et le tonus
-              des personnes qui y vivent soient améliorés.
-            </p>
-            <hr></hr>
-          </article>
-
-          <article className="article">
-            <p>
-              Vous pouvez déposer ici vos avis, commentaires et remarques sur le
-              travail que j’ai effectué pour vous.
-            </p>
-          </article>
-
+          <Index_content></Index_content>
           <List_review></List_review>
           <User_review></User_review>
         </section>
