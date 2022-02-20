f9e9b3a3b32fc388da2be797545f612ea74b4f78
raficraft <raficraft@gmail.com>
2022-02-20 22:00:03 +0100

    test bug parsing content on prod

--- pages/menhir/Menhir_content.js
+++ pages/menhir/Menhir_content.js
@@ -4,18 +4,7 @@ import HR from "../../engine/component/atomic/HR";
 export default function Menhir_content() {
   return (
     <>
-      <article className="article article_home">
-        <header>
-          <h2>Pose de menhir et pierres levée</h2>
-        </header>
-      </article>
-
-      <HR
-        css={{
-          margin: "4 0",
-          width: "100%",
-        }}
-      ></HR>
+      <h1>GGGR</h1>
     </>
   );
 }
