0bc326567f8dee3f003ca918de7322568d98f8ad
raficraft <raficraft@gmail.com>
2022-02-21 18:22:38 +0100

    write content pages home

--- pages/Index_content.js
+++ pages/Index_content.js
@@ -24,47 +24,62 @@ export default function Index_content() {
         </div>
         <p>Cheminement vers mon vrai chemin de vie</p>
         <p>
-          {
-            "La traversée d'une épreuve difficile m'a ouvert une nouvelle vision sur le monde qui nous entoure. J'ai dû faire face à ma sensibilité dans la dimension énergétique."
-          }
+          La traversée d&rsquo;une épreuve difficile m&rsquo;a ouvert une
+          nouvelle vision sur le monde qui nous entoure. J&rsquo;ai dû faire
+          face à ma sensibilité dans la dimension énergétique.
         </p>
 
         <p>
-          {
-            " Quand ce nouveau monde a commencé à s'ouvrir à moi, j'ai ressenti le  mal être des personnes que je croisais, qui travaillaient autour de moi, les faiblesses liées à l'énergétiques. Ma clairvoyance, claire sentence et claire audience se sont développées à ce moment-là."
-          }
+          Quand ce nouveau monde a commencé à s&rsquo;ouvrir à moi, j&rsquo;ai
+          ressenti le mal être des personnes que je croisais, qui travaillaient
+          autour de moi, les faiblesses liées à l&rsquo;énergétiques. Ma
+          clairvoyance, claire sentence et claire audience se sont développées à
+          ce moment-là.
+        </p>
+        <p>
+          "Après avoir travaillé dans différentes entreprises, je me suis
+          éloigné de ce système qui ne me correspond pas : courses à
+          l&rsquo;argent, rythme de vie effréné, égoïsme... Je me consacre
+          aujourd&rsquo;hui entièrement à l&rsquo;ésotérisme.
+        </p>
+        <p>
+          "Mes nombreuses lectures, des formations (géobiologie, soins
+          énergétiques, chamanisme sibérien, runes) et l&rsquo;aide de personnes
+          sur mon chemin (guérisseuse, prêtre alchimiste) m&rsquo;ont permis de
+          comprendre que ma place était ailleurs et que rien n&rsquo;est lié au
+          hasard.
+        </p>
+        <p>
+          J&rsquo;ai décidé de réaliser mes soins sur les personnes,
+          harmonisations de lieux à distance et dégagements de toutes sortes de
+          magies dans un zome énergétique. Cette structure est une forme
+          géométrique composée de losanges agencés en double spirale dont la
+          trame se veut en résonance avec le monde vivant.
+        </p>
+        <p>
+          Après plusieurs années d&rsquo;approfondissement dans ce domaine afin
+          d&rsquo;être à la hauteur de ce qui m&rsquo;est permis de réaliser, je
+          suis aujourd&rsquo;hui pleinement inscrit dans mon chemin de vie.
+        </p>
+        <p>
+          Faisant partie de la Fédération Française de Géobiologie, je
+          m&rsquo;inscris au même titre que de nombreux autres géobiologues dans
+          la charte de la FFG nous engageant à exercer notre activité avec
+          objectivité, humanité et probité.
         </p>
         <p>
           {
-            "Après avoir travaillé dans différentes entreprises, je me suis éloigné de ce système qui ne me correspond pas : courses à l'argent, rythme de vie effréné, égoïsme... Je me consacre aujourd'hui entièrement à l'ésotérisme."
-          }
-        </p>
-        <p>
-          {
-            "Mes nombreuses lectures, des formations (géobiologie, soins énergétiques, chamanisme sibérien, runes) et l'aide de personnes sur mon chemin (guérisseuse, prêtre alchimiste) m'ont permis de comprendre que ma place était ailleurs et que rien n'est lié au hasard."
-          }
-        </p>
-        <p>
-          {
-            " J'ai décidé de réaliser mes soins sur les personnes, harmonisations de lieux à distance et dégagements de toutes sortes de magies dans un zome énergétique. Cette structure est une forme géométrique composée de losanges agencés en double spirale dont la trame se veut en résonance avec le monde vivant. "
-          }
-        </p>
-        <p>
-          {
-            "Après plusieurs années d'approfondissement dans ce domaine afin d'être à la hauteur de ce qui m'est permis de réaliser, je suis aujourd'hui pleinement inscrit dans mon chemin de vie. "
-          }
-        </p>
-        <p>
-          {
-            "Faisant partie de la Fédération Française de Géobiologie, je m'inscris au même titre que de nombreux autres géobiologues dans la charte de la FFG nous engageant à exercer notre activité avec objectivité, humanité et probité."
-          }
-        </p>
-        <p>
-          {
-            "Mon seul but est d'accompagner les personnes venant vers moi à trouver des solutions à leur mal être, et leur permettre de retrouver un état de bien être dans leur vie. "
+            "Mon seul but est d&rsquo;accompagner les personnes venant vers moi à trouver des solutions à leur mal être, et leur permettre de retrouver un état de bien être dans leur vie. "
           }
         </p>
       </article>
+
+      <HR
+        css={{
+          margin: "4 0",
+          width: "100%",
+        }}
+      ></HR>
 
       <article className="article article_home">
         <h2>Revue de presse</h2>
@@ -103,7 +118,7 @@ export default function Index_content() {
               </Link>
             </div>
             <p className="text-center">
-              {" Document Pdf. Cliquer sur l'image pour consulter"}
+              {" Document Pdf. Cliquer sur l&rsquo;image pour consulter"}
             </p>
           </div>
 
@@ -114,14 +129,20 @@ export default function Index_content() {
             <div className="media_home-radio--player">
               <figure>
                 <figcaption>Reportage David Michel</figcaption>
-                <audio controls src="/media/cc0-audio/t-rex-roar.mp3">
+                <audio
+                  controls
+                  src="gs://geobiologie-loire.appspot.com/interview_david_michel.mp3"
+                >
                   Your browser does not support the
                   <code>audio</code> element.
                 </audio>
               </figure>
               <figure>
                 <figcaption>Recontre avec David Michel</figcaption>
-                <audio controls src="/media/cc0-audio/t-rex-roar.mp3">
+                <audio
+                  controls
+                  src="gs://geobiologie-loire.appspot.com/recontre_dav_dmichel.mp3"
+                >
                   Your browser does not support the
                   <code>audio</code> element.
                 </audio>
@@ -130,6 +151,13 @@ export default function Index_content() {
           </div>
         </div>
       </article>
+
+      <HR
+        css={{
+          margin: "4 0",
+          width: "100%",
+        }}
+      ></HR>
     </>
   );
 }
