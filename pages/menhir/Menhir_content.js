91b9f496f1e36e001056fcb5dcd2043708d2cb71
raficraft <raficraft@gmail.com>
2022-02-21 08:20:34 +0100

    fix video link and the string authorized in vercel hosting step2

--- pages/menhir/Menhir_content.js
+++ pages/menhir/Menhir_content.js
@@ -8,23 +8,6 @@ export default function Menhir_content() {
         <header>
           <h2>Pose de menhir et pierres levée</h2>
         </header>
-        <p>
-          Un menhir est comme une antenne, il sert à capter l'énergie du lieu et
-          à la faire rayonner autour de lui. Il crée une alchimie entre Terre et
-          Ciel (tellurique-cosmique). Poser un menhir permet de dynamiser les
-          terrains perturbés et de repousser des réseaux négatifs.
-        </p>
-        <p>
-          La géo poncture (pose de pierres levées ou menhirs) permet aussi de
-          finaliser le rééquilibrage énergétique de la maison et du lieu. D'une
-          aide efficace dans les habitations, le menhir devient un outils
-          indispensable dans les lieux tel que les exploitations agricoles et
-          les maisons de grandes superficies.
-        </p>
-        <p>
-          Un menhir a aussi pour rôle de contrer les ondes de formes créer par
-          les éoliennes, antennes relais, poteaux électriques mal positionnés.
-        </p>
       </article>
 
       <HR
