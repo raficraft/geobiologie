d6daf80443fc455274d4f35d1a216d22c594e563
raficraft <raficraft@gmail.com>
2022-02-21 15:29:12 +0100

    test escape simple quotes in string step 8

--- next.config.js
+++ next.config.js
@@ -3,26 +3,6 @@ module.exports = {
 
   extends: "next",
   rules: {
-    "react/no-unescaped-entities": ["error", { forbid: [">", "}", "'"] }],
-    // or
-    "react/no-unescaped-entities": [
-      "error",
-      {
-        forbid: [
-          {
-            char: ">",
-            alternatives: ["&gt;"],
-          },
-          {
-            char: "}",
-            alternatives: ["&#125;"],
-          },
-          {
-            char: "'",
-            alternatives: ["&rsquo;"],
-          },
-        ],
-      },
-    ],
+    "react/no-unescaped-entities": "off",
   },
 };
