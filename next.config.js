ce2e13cb3cd1720d7d5a16a1039b78120ffc7401
raficraft <raficraft@gmail.com>
2022-02-21 17:15:01 +0100

    write content pages sourcier

--- next.config.js
+++ next.config.js
@@ -2,5 +2,26 @@ module.exports = {
   reactStrictMode: true,
 
   extends: "next",
-  rules: { "react/no-unescaped-entities": 0 },
+  rules: {
+    // or
+    "react/no-unescaped-entities": [
+      "error",
+      {
+        forbid: [
+          {
+            char: ">",
+            alternatives: ["&gt;"],
+          },
+          {
+            char: "}",
+            alternatives: ["&#125;"],
+          },
+          {
+            char: "'",
+            alternatives: ["&rsquo;"],
+          },
+        ],
+      },
+    ],
+  },
 };
--- pages/sourcier/Sourcier_content.js
+++ pages/sourcier/Sourcier_content.js
@@ -16,33 +16,57 @@ export default function Sourcier_content() {
           courant de l’eau. Pour cela, je travaille avec des baguettes en Y et
           en L, mais aussi avec mon pendule et diagrammes.
         </p>
-        <h3>L{"'"}eau souterraine</h3>
+        <h3>L&rsquo;eau souterraine</h3>
         <p>
-          {` Pas la peine de vous faire un dessin, tout le monde voit ici à quoi
-          ressemble un cours <b>d'eau souterrain</b>, avec lequel nous inclusons les
-          sources, les nappes et les lacs souterrain. Cest cours d{"'"}eau
+          Pas la peine de vous faire un dessin, tout le monde voit ici à quoi
+          ressemble un cours deau souterrain, avec lequel nous inclusons les
+          sources, les nappes et les lacs souterrain. Cest cours d&rsquo;eau
           souterrains peuvent être petits comme un ruisseau ou grand comme un
-          fleuve ou un lac`}
+          fleuve ou un lac.
         </p>
         <p>
           Les <b>actions</b> sur les êtres vivants sont différents selon la
-          profondeur, la largeur, la composition et dans les cas de cours d{"'"}
-          eau, le débit.
+          profondeur, la largeur, la composition et dans les cas de cours
+          d&rsquo;eau, le débit.
         </p>
 
         <ul>
           <li>
-            <b>La profondeur : </b> les scientifiques considèrent qu{"'"}un
-            cours d{"'"}eau souterrain a une influence nocive à partir de 7
+            <b>La profondeur : </b> les scientifiques considèrent qu&rsquo;un
+            cours d&rsquo;eau souterrain a une influence nocive à partir de 7
             mètres de profondeur environ
           </li>
 
           <li>
-            <b>La profondeur : </b> les scientifiques considèrent qu{"'"}un
-            cours d{"'"}eau souterrain a une influence nocive à partir de 7
-            mètres de profondeur environ
+            <b>La largeur : </b> la largeur d&rsquo;un cours d&rsquo;eau
+            souterain influera sur une largeur au moins autant plus importante
+            sur la surface terrestre. Cependant, une influence peut être
+            déterminée à la surface sur une distance égale à la profondeur de 12
+            mètres, l&rsquo;influence sera détecté 12 mètres avant le millieu du
+            cours d&rsquo;eau. Pour faciliter les recherces, nous demandons
+            mentalement le point de force.
+          </li>
+          <li>
+            <b>La composition :</b> nous nous contenterons de déterminer si
+            cette eau souterraine est pollué ou non. Nous pouvons aussi
+            déterminer sa composition minérale, la nature des roces traversée...
+          </li>
+
+          <li>
+            <b>Le débit :</b> plus le débit est important, plus la nocivité sera
+            grande et dangereuse pour tout ce qui vit à la surface terrestre.
+            C&rsquo;est comme un courant éléctrique dû au frottement de
+            l&rsquo;eau sur sa gaine naturelle. La nocivité peut changer selon
+            la nature du terrain au niveau du cours d&rsquo;eau
           </li>
         </ul>
+        <h3>Mémoire de l&rsquo;eau</h3>
+        <p>
+          L&rsquo;eau à une mémoire. Il peut arriver qu&rsquo;en passant sur un
+          courant d&rsquo;eau souterrain il n&rsquo;y ait plus d&rsquo;eau, mais
+          la mémoire de cette eau est toujours là : le pendule la détecte donc
+          toujours.
+        </p>
       </article>
 
       <HR
