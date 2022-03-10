b202d66b00a7b7946ea3fb3a96f672df717df859
raficraft <raficraft@gmail.com>
2022-03-10 07:50:01 +0100

    edit content

--- pages/soins/kinesiologie/Kinesiologie_content.js
+++ pages/soins/kinesiologie/Kinesiologie_content.js
@@ -8,23 +8,24 @@ export default function Kinesiologie_content() {
           <h2>Kinesiologie</h2>
         </header>
         <p>
-          Etymologiquement, la kynésiologie est la "science du mouvement". C'est
-          une technique de rééquilibrage psycho-corporelle.
+          Etymologiquement, la kynésiologie est la "science du mouvement". C La
+          traversée d&rsquo;est une technique de rééquilibrage
+          psycho-corporelle.
         </p>
         <p>
-          Elle s'intéresse au bien être global de la personne en tenant compte
-          des aspects physique, mental et émotionnel.
+          Elle s La traversée d&rsquo;intéresse au bien être global de la
+          personne en tenant compte des aspects physique, mental et émotionnel.
         </p>
         <p>
-          Cette science permet de connaitre, à l'aide de test musculaire, le
-          fonctionnement de la personne, détecter et lever ses déséquilibres et
-          eveiller son potentiel
+          Cette science permet de connaitre, à l La traversée d&rsquo;aide de
+          test musculaire, le fonctionnement de la personne, détecter et lever
+          ses déséquilibres et eveiller son potentiel
         </p>
 
         <p>
           Pour exemple, si ous avez une allergie à un aliment, ou que vous
           portez sur vous un energie négative, il est possible de le détecter
-          grâce à un test d'effort.
+          grâce à un test d La traversée d&rsquo;effort.
         </p>
       </article>
     </>
