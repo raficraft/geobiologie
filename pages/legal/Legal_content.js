219c5c52e3d391f9fdc042fce86539cbb9c58e57
raficraft <raficraft@gmail.com>
2024-06-17 10:20:44 +0200

    fix bug

--- pages/legal/Legal_content.js
+++ pages/legal/Legal_content.js
@@ -11,7 +11,7 @@ export default function Legal_content() {
           <ul>
             <li>DAVID-MICHEL - Géobiologue</li>
             <li>125 chemin du moulin</li>
-            <li>38410 Saint-Martin d'uriage</li>
+            <li>38410 Saint-Martin d&apos;uriage</li>
             <li>TEL : 06.58.02.17.24</li>
             <li>N SIRET : 84996618900011</li>
           </ul>
@@ -21,53 +21,56 @@ export default function Legal_content() {
           <h3>Informatique et libertés</h3>
           <p>
             Les données personnelles relatives aux clients, recueillies par
-            l'intermédiaire des différents formulaires ne sont destinées qu'à un
-            usage exclusif de David Michel dans le cadre de la mise en œuvre de
-            ses services. David Michel s'engage à maintenir strictement
-            confidentielles les données personnelles recueillies. Conformément
-            aux dispositions de la Loi n° 78-17 du 6 janvier 1978 relative à
-            l'informatique, aux fichiers et aux libertés, les utilisateurs
-            disposent suivant l'Article 34 de la Loi du 6 janvier 1978 d'un
-            droit d'accès aux données qui les concernent, ainsi que d'un droit
-            de modification et de suppression de celles-ci. Il leur suffit, pour
-            exercer ce droit, d'adresser un courrier postal ou un e-mail à David
-            Michel aux coordonnées ci-dessus.
+            l&apos;intermédiaire des différents formulaires ne sont destinées
+            qu&apos;à un usage exclusif de David Michel dans le cadre de la mise
+            en œuvre de ses services. David Michel s&apos;engage à maintenir
+            strictement confidentielles les données personnelles recueillies.
+            Conformément aux dispositions de la Loi n° 78-17 du 6 janvier 1978
+            relative à l&apos;informatique, aux fichiers et aux libertés, les
+            utilisateurs disposent suivant l&apos;Article 34 de la Loi du 6
+            janvier 1978 d&apos;un droit d&apos;accès aux données qui les
+            concernent, ainsi que d&apos;un droit de modification et de
+            suppression de celles-ci. Il leur suffit, pour exercer ce droit,
+            d&apos;adresser un courrier postal ou un e-mail à David Michel aux
+            coordonnées ci-dessus.
           </p>
         </div>
 
         <div>
           <h3>Propriété intellectuelle</h3>
           <p>
-            L'utilisateur s'engage à ne pas reproduire, résumer, modifier,
-            altérer ou rediffuser, sans autorisation expresse préalable de
-            l'éditeur, quelque texte, titre, application, logo, marque,
-            information ou illustration, pour un usage autre que strictement
-            privé, ce qui exclut toute représentation à des fins
-            professionnelles ou de rediffusion en nombre. De même, l'utilisateur
-            s'engage à ne pas recopier tout ou partie du site sur un autre site
-            ou un réseau interne d'entreprise.
+            L&apos;utilisateur s&apos;engage à ne pas reproduire, résumer,
+            modifier, altérer ou rediffuser, sans autorisation expresse
+            préalable de l&apos;éditeur, quelque texte, titre, application,
+            logo, marque, information ou illustration, pour un usage autre que
+            strictement privé, ce qui exclut toute représentation à des fins
+            professionnelles ou de rediffusion en nombre. De même,
+            l&apos;utilisateur s&apos;engage à ne pas recopier tout ou partie du
+            site sur un autre site ou un réseau interne d&apos;entreprise.
           </p>
         </div>
         <div>
-          <h3>Conditions d'utilisation du site</h3>
+          <h3>Conditions d&apos;utilisation du site</h3>
           <p>
-            Vous consultez actuellement le site de l'entreprise de Géobiologie
-            de David Michel. L'utilisation de ce site vaut acceptation des
-            conditions d'utilisation présentes. Contact pour toute question sur
-            l'utilisation du site : Gmail geobiologie
+            Vous consultez actuellement le site de l&apos;entreprise de
+            Géobiologie de David Michel. L&apos;utilisation de ce site vaut
+            acceptation des conditions d&apos;utilisation présentes. Contact
+            pour toute question sur l&apos;utilisation du site : Gmail
+            geobiologie
           </p>
         </div>
         <div>
-          <h3>Les formulaires d'envoi</h3>
+          <h3>Les formulaires d&apos;envoi</h3>
           <p>
-            Les messages adressés à ce site par le biais de formulaire d'envoi
-            sont utilisés à la seule fin du traitement du message, et ne seront
-            pas communiqués à des tiers sans l'accord de l'émetteur. Ces
-            messages seront conservés pour la durée de traitement de la question
-            posée. Le site Internet de David Michel n'offrant pas de dispositif
-            particulier de sécurisation (transfert de fichiers, transfert de
-            données), l'expéditeur d'une question ou d'un document confidentiel
-            ou sensible devra privilégier la voie postale.
+            Les messages adressés à ce site par le biais de formulaire
+            d&apos;envoi sont utilisés à la seule fin du traitement du message,
+            et ne seront pas communiqués à des tiers sans l&apos;accord de
+            l&apos;émetteur. Ces messages seront conservés pour la durée de
+            traitement de la question posée. Le site Internet de David Michel
+            n&apos;offrant pas de dispositif particulier de sécurisation
+            (transfert de fichiers, transfert de données), l&apos;expéditeur
+            d&apos;une question ou d&apos;un document confidentiel ou sensible
+            devra privilégier la voie postale.
           </p>
         </div>
 
@@ -75,18 +78,20 @@ export default function Legal_content() {
           <h3>Les liens hypertextes</h3>
           <p>
             La création de tout lien hypertexte en direction du site devra faire
-            l'objet d'une autorisation expresse préalable du directeur de la
-            publication. Cette autorisation préalable vaut aussi bien pour la
-            création :
+            l&apos;objet d&apos;une autorisation expresse préalable du directeur
+            de la publication. Cette autorisation préalable vaut aussi bien pour
+            la création :
           </p>
           <ul>
             <li>
-              D'un lien hypertexte sur la page de garde dans une nouvelle
+              D&apos;un lien hypertexte sur la page de garde dans une nouvelle
               fenêtre du navigateur,
             </li>
-            <li>D'un lien vers une page intérieure du site,</li>
-            <li>D'un lien à travers des fenêtres (framing),</li>
-            <li>d'un lien vers un objet contenu dans le site (inlining)</li>
+            <li>D&apos;un lien vers une page intérieure du site,</li>
+            <li>D&apos;un lien à travers des fenêtres (framing),</li>
+            <li>
+              d&apos;un lien vers un objet contenu dans le site (inlining)
+            </li>
             <li>
               et plus généralement de toute technique de création de lien
               hypertexte.
